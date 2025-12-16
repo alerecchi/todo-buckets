27493568d114fce99cd70a78c5cde3232f435868
Alessandro Recchi <ale.recchi@gmail.com>
2025-12-16 17:11:19 +0100

    Plan update and mock data cleanup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { Todo } from '@/types/Todo'
-import { Bucket } from '@/types/Bucket'
 import { BucketList } from '@/components/features/BucketList'
+import { bucketList } from '@/lib/mockData'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-  const todoItem: Todo = {
-    id: 1,
-    title: 'Sample Todo',
-    description: 'This is a sample todo item',
-    completed: false,
-  }
-  const bucketItem: Bucket = {
-    id: 1,
-    name: 'Sample Bucket',
-    todos: [todoItem, todoItem],
-  }
   return (
     <div>
-      <BucketList bucketList={[bucketItem, bucketItem, bucketItem, bucketItem, bucketItem]} />
+      <BucketList bucketList={bucketList} />
     </div>
   )
 }
