745d5c61e4025d7fffa6cd9e999a3da13a66f6b6
Alessandro Recchi <ale.recchi@gmail.com>
2025-12-24 13:41:51 +0100

    Dialog for adding todos (#12)

--- src/features/todos/components/BucketColumn.tsx
+++ src/features/todos/components/BucketColumn.tsx
@@ -1,24 +1,38 @@
-import { Inbox } from "lucide-react"
+import { Inbox, Plus } from 'lucide-react'
 import TodoCard from './TodoCard'
 import { Bucket } from '@/types/Bucket'
+import { Button } from '@/features/shared/components/ui/button'
+import AddTodoDialog from './AddTodoDialog'
+import { useState } from 'react'
 
 interface BucketProps {
   bucket: Bucket
 }
 
 export function BucketColumn({ bucket }: BucketProps) {
+  let [isDialogOpen, setDialogOpen] = useState(false)
+
   return (
     <div className="flex flex-col gap-4 min-h-[100px] p-2 rounded-lg border-2 border-dashed border-border bg-muted/20">
-      <div className="flex flex-row gap-2 items-center p-2">
-        <Inbox />
-        <h2 className="text-lg font-semibold">{bucket.name}</h2>
+      <div className="flex items-center justify-between">
+        <div className="flex flex-row gap-2 items-center p-2">
+          <Inbox />
+          <h2 className="text-lg font-semibold">{bucket.name}</h2>
         </div>
+        <Button variant="ghost" size="sm" onClick={() => setDialogOpen(true)}>
+          <Plus className="h-4 w-4" />
+        </Button>
+        <AddTodoDialog
+          open={isDialogOpen}
+          setOpen={setDialogOpen}
+          addTodo={(text) => console.log(text)}
+        />
+      </div>
       <div className="flex flex-col gap-2">
         {bucket.todos.map((todoItem) => (
-        <TodoCard todo={todoItem} />
-      ))}
+          <TodoCard key={todoItem.id} todo={todoItem} />
+        ))}
       </div>
-      
     </div>
   )
 }
